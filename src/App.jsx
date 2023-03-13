19559fe052ca467d4d31af64290391c69617cc27
unknown <thravshanovich6356@gmail.com>
2023-03-13 12:40:47 +0300

    completed full timer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Timer from './components/timer/Timer';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
